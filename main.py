af8a429c10c294f3bdde792aa58de1b16a7ecae4
Nazarova_Vladislava <nvv.03@yandex.ru>
2023-08-09 10:29:26 +0700

    открывает фикбук и берет название фф по заданной ссылке

---
+++ main.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome()
+
+browser.get('https://ficbook.net/readfic/12311968?fragment=part_content')
+
+a = browser.find_element(By.CSS_SELECTOR, "h1.mb-10")
+
+print(a.text)
