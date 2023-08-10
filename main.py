2d193b20a28af2eb3b7b84a3d0e50c90127fd850
Nazarova_Vladislava <nvv.03@yandex.ru>
2023-08-10 23:23:37 +0700

    ошибка.. ничего не работает пришлось переустанавливать селениум

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ browser.get('https://ficbook.net/readfic/12311968?fragment=part_content')
 a = browser.find_element(By.CSS_SELECTOR, "h1.mb-10")
 
 print(a.text)
+
+chapter_links = browser.find_elements(By.CLASS_NAME, "part")
+
