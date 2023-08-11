6b1e8123e065b39ad156376eb5ff4421bc5f3fd0
Nazarova_Vladislava <nvv.03@yandex.ru>
2023-08-11 14:15:05 +0700

    версия не идеальна но стабильна. все свои функции выполняет

--- main.py
+++ main.py
@@ -10,40 +10,39 @@ browser = webdriver.Chrome()
 book_url = 'https://ficbook.net/readfic/10684982'
 browser.get(book_url)
 
-time.sleep(50)
+time.sleep(4)
 
 title = browser.find_element(By.CSS_SELECTOR, "h1.mb-10")
-
-chapter_links = browser.find_elements(By.CLASS_NAME, "part")
-
 book = epub.EpubBook()
 book.set_title("" + title.text)
 book.set_language("ru")
 
+chapter_links = browser.find_elements(By.CLASS_NAME, "part")
 # Перебираем ссылки на главы
-for index, link in enumerate(chapter_links):
-    chapter_title = link.find_element(By.CSS_SELECTOR, "div.part-title h3").text
-    chapter_url = link.find_element(By.TAG_NAME, "a").get_attribute("href")
+for index in range(len(chapter_links)):
+    chapter_links = browser.find_elements(By.CLASS_NAME, "part")
+    chapter_title = chapter_links[index].find_element(By.CSS_SELECTOR, "h3").text
 
     # Переходим на страницу главы
-    browser.get(chapter_url)
-    time.sleep(20)
+    browser.delete_all_cookies()
+    time.sleep(3)
+    chapter_links[index].click()
 
     # Ожидание, пока элемент с id "content" станет видимым
-    wait = WebDriverWait(browser, 10)  # Максимальное время ожидания в секундах
+    wait = WebDriverWait(browser, 5)
     chapter_text_element = wait.until(EC.visibility_of_element_located((By.ID, "content")))
 
     # Находим элемент с текстом главы и извлекаем текст
-    chapter_text_element = browser.find_element(By.ID, "content")
+    # вот тут можно доработать потому что все форматирование уезжает
     chapter_text = chapter_text_element.text
 
     # Создаем объект главы EPUB
     chapter = epub.EpubHtml(title=chapter_title, file_name=f"chap_{index}.xhtml", lang="ru")
-
-    # Добавляем текст главы в объект главы EPUB
     chapter.content = f"<h1>{chapter_title}</h1><br>{chapter_text}".encode("utf-8")
     book.add_item(chapter)
     book.spine.append(chapter)
+
+    browser.back()
 
 # Создаем EPUB-файл
 epub.write_epub("1.epub", book, {})
