1c13b7d6de988324dfa6e152a323a5b330a86c74
Nazarova_Vladislava <nvv.03@yandex.ru>
2023-08-11 16:18:30 +0700

    попытка обойти капчу не удалась. не прогружает последнюю страницу из 3

--- main.py
+++ main.py
@@ -2,19 +2,28 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 from ebooklib import epub
+import undetected_chromedriver as uc
 
-browser = webdriver.Chrome()
 
+options = webdriver.ChromeOptions()
+# options.add_argument("start-maximized")
+# options.add_experimental_option("excludeSwitches", ["enable-automation"])
+# options.add_experimental_option('useAutomationExtension', False)
+# browser = uc.Chrome(options=options)
+browser = uc.Chrome(headless=False,use_subprocess=True, options=options)
+
+# browser = webdriver.Chrome()
+# browser.delete_all_cookies()
 book_url = 'https://ficbook.net/readfic/10684982'
 browser.get(book_url)
 
-time.sleep(4)
+wait = WebDriverWait(browser, 35)
+title = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "h1.mb-10")))
 
-title = browser.find_element(By.CSS_SELECTOR, "h1.mb-10")
+# title = browser.find_element(By.CSS_SELECTOR, "h1.mb-10")
 book = epub.EpubBook()
-book.set_title("" + title.text)
+book.set_title(title.text)
 book.set_language("ru")
 
 chapter_links = browser.find_elements(By.CLASS_NAME, "part")
@@ -25,7 +34,6 @@ for index in range(len(chapter_links)):
 
     # Переходим на страницу главы
     browser.delete_all_cookies()
-    time.sleep(3)
     chapter_links[index].click()
 
     # Ожидание, пока элемент с id "content" станет видимым
@@ -38,11 +46,11 @@ for index in range(len(chapter_links)):
 
     # Создаем объект главы EPUB
     chapter = epub.EpubHtml(title=chapter_title, file_name=f"chap_{index}.xhtml", lang="ru")
-    chapter.content = f"<h1>{chapter_title}</h1><br>{chapter_text}".encode("utf-8")
+    chapter.set_content(f"<h1>{chapter_title}</h1><br><p>{chapter_text}</p>")
     book.add_item(chapter)
     book.spine.append(chapter)
 
     browser.back()
 
 # Создаем EPUB-файл
-epub.write_epub("1.epub", book, {})
+epub.write_epub("2.epub", book, {})
